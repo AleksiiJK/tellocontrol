db3b8ff77a1a9f27509702b940afbe81d089a365
Eero Virtanen <eeavir@utu.fi>
2025-03-27 17:32:05 +0200

    Tällä saatiin kuva näkymään

---
+++ src/own_package/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'own_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='eeavir',
+    maintainer_email='eeavir@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'image_subscriber = own_package.image_subscriber:main',
+        ],
+    },
+)
