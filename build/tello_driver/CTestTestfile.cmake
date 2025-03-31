201d8c62f19d9d46874a2d425d73b8ac1997e620
Supernakki <supernakki01@gmail.com>
2025-03-31 15:13:15 +0300

    Siisti testi

---
+++ build/tello_driver/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/atte/drone_ws/src/drone_racing_ros2/tello_ros/tello_driver
+# Build directory: /home/atte/drone_ws/build/tello_driver
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
