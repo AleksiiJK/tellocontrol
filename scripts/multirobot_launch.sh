766e3a9a3fbb84fef698f1ffc1098cba3f09d920
AleksiiJK <aleksikytoviita@gmail.com>
2025-05-19 14:34:20 +0300

    Added launch script that builds and launches all the relevant  packages with proper args

---
+++ scripts/multirobot_launch.sh
@@ -0,0 +1,15 @@
+# Build the packages. In case of WSL use option "--parallel-workers 1" to avoid failure due to lack of memory 
+# This is not necessary on native Ubuntu, and slows down the build process
+colcon build --parallel-workers 1 
+# Source the build
+source install/setup.bash
+# Launch the world spawn a create3 robot. Disregard Rviz and the dock. Offset start y-position by 3 meters to give space for the drone 
+ros2 launch irobot_create_gazebo_bringup create3_gazebo.launch.py use_rviz:='false' spawn_dock:='false' y:='3' &
+# Small delay for stability
+sleep 1.5 
+# Add the tello-drone to the simulation 
+ros2 launch tello_gazebo tello_launch.py &
+# Another small delay
+sleep 1.5
+# Launch the controller 
+# ros2 launch.. 
